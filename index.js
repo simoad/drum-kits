40b3487d4aed0893dc8e3117604ac53137918138
simoad <48855843+simoad@users.noreply.github.com>
2021-02-07 16:20:50 +0100

    source code v1.0

---
+++ index.js
@@ -0,0 +1,70 @@
+var numberOfButton = document.querySelectorAll("button").length;
+
+for (var i=0; i<numberOfButton; i++) {
+    document.querySelectorAll("button")[i].addEventListener('click', function() {
+
+        var buttonInnerHTML = this.innerHTML;
+
+        // Play Convenable Sound
+        makeSound(buttonInnerHTML);
+        // Add Pressed Animation
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+document.addEventListener('keydown', function(event) {
+    // Play Convenable Sound
+    makeSound(event.key);
+    // Add Pressed Animation
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case 'w': 
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break;
+
+        case 'a': 
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+        break;
+
+        case 's': 
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+
+        case 'd': 
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+
+        case 'j': 
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+        break;
+
+        case 'k': 
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+
+        case 'l': 
+        var kick = new Audio('sounds/kick-bass.mp3');
+        kick.play();
+        break;
+
+    }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100)
+}
